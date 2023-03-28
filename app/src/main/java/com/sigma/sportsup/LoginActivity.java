6c4129bdc34b72b7a6ee558e490443b221fdfc11
Ashwin R Nair <85173652+AshwinRNair-ARN@users.noreply.github.com>
2023-03-28 14:07:39 +0530

    removed skip button

--- app/src/main/java/com/sigma/sportsup/LoginActivity.java
+++ app/src/main/java/com/sigma/sportsup/LoginActivity.java
@@ -290,8 +290,4 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
     }
 
-    public void onSkipLogin(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
 }
