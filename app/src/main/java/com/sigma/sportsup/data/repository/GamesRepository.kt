6e120bd19eb3e92883e2bdcf16022402cecdb910
Antonio Pedro <tonio.pedro99@gmail.com>
2023-04-12 22:00:53 +0530

    Feat: add Games to venues, added game duration, added players to game

---
+++ app/src/main/java/com/sigma/sportsup/data/repository/GamesRepository.kt
@@ -0,0 +1,6 @@
+package com.sigma.sportsup.data.repository
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.sigma.sportsup.data.GameModel
+import kotlinx.coroutines.flow.Flow
+
--- app/src/main/java/com/sigma/sportsup/ui/game_creation/GameCreateFragment.kt
+++ app/src/main/java/com/sigma/sportsup/ui/game_creation/GameCreateFragment.kt
@@ -3,6 +3,7 @@ package com.sigma.sportsup.ui.game_creation
 import android.R
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -25,14 +26,14 @@ import com.sigma.sportsup.ui.events.EventsViewModel
 import java.text.SimpleDateFormat
 import java.util.*
 
-class GameCreateFragment:Fragment() {
+class GameCreateFragment : Fragment() {
 
 
-    private var _binding : FragmentGameCreateFragmentBinding? = null
+    private var _binding: FragmentGameCreateFragmentBinding? = null
     private val binding get() = _binding!!
-    private lateinit var mContext :Context
+    private lateinit var mContext: Context
 
-    private lateinit var user:UserModel
+    private lateinit var user: UserModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -48,12 +49,12 @@ class GameCreateFragment:Fragment() {
 
         }
 
-        gameCreateViewModel.games.observe(viewLifecycleOwner) {it ->
+        gameCreateViewModel.games.observe(viewLifecycleOwner) { it ->
             val games = it?.map { it.name }
             buildGamesItems(games!!)
         }
 
-        gameCreateViewModel.currentUser.observe(viewLifecycleOwner){ user = it }
+        gameCreateViewModel.currentUser.observe(viewLifecycleOwner) { user = it }
         val root: View = _binding!!.root
         return root
     }
@@ -71,30 +72,37 @@ class GameCreateFragment:Fragment() {
         val edtNumberOfPlayers = binding.edtNumberPlayers
         val edtDate = binding.edtiDate
         val edtTime = binding.editTime
+        val edtDurationFormat = binding.editDurationFormat
+        val edtDuration = binding.edtDuration
 
         selectDate()
         selectTime()
         var audience = "Everyone"
 
         rgAudience.setOnCheckedChangeListener { _, checkedId ->
-            when(checkedId){
+            when (checkedId) {
                 binding.rbEveryone.id -> audience = binding.rbEveryone.text.toString()
-                binding.rbMyFriends.id -> audience=  binding.rbMyFriends.text.toString()
-        } }
+                binding.rbMyFriends.id -> audience = binding.rbMyFriends.text.toString()
+            }
+        }
 
+        buildUnitOfTime()
+        showAvailableVenues()
         btnInvite.setOnClickListener {
             val db = Firebase.firestore
 
             val event = hashMapOf(
                 "name" to edtGame.text.toString(),
                 "host" to user.name,
-                "host_ref" to  user.id,
+                "host_ref" to user.id,
                 "venue" to edtVenue.text.toString(),
                 "date" to edtDate.text.toString(),
                 "time" to edtTime.text.toString(),
                 "current_players" to 1,
+                "status" to "planed",
                 "number_of_players" to edtNumberOfPlayers.text.toString(),
-                "audience" to audience
+                "audience" to audience,
+                "duration" to "${edtDuration.text.toString()} ${edtDurationFormat.text.toString()}"
             )
 
             db.collection("games")
@@ -102,12 +110,50 @@ class GameCreateFragment:Fragment() {
                 .collection("items")
                 .add(event)
                 .addOnSuccessListener { doc ->
-                    doc.collection("players") //add the current user to players list
+
+                    //add a game snapshot to the venues
+                    db.collection("venues").whereEqualTo("name", event["venue"]).get()
+                        .addOnSuccessListener {
+                            // Toast.makeText(requireContext(), it.documents.toString(), Toast.LENGTH_LONG).show()
+                            for (document in it) {
+                                val gameSessionVenue = hashMapOf(
+                                    "session" to doc.id,
+                                    "date" to edtDate.text.toString(),
+                                    "time" to event["time"],
+                                    "status" to event["status"],
+                                    "duration" to event["duration"]
+                                )
+                                val ref = document.reference.collection("games").document()
+                                ref.set(gameSessionVenue).addOnFailureListener { exception ->
+                                    Toast.makeText(
+                                        requireContext(),
+                                        exception.message,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                                    .addOnSuccessListener {
+                                        print("Done")
+                                    }
+                            }
+                        }
+
+                    //add players to game, the first player will always be the host
+                    doc.collection("players").add(
+                        hashMapOf(
+                            "name" to user.name,
+                            "id" to user.id
+                        )
+                    )
+                        .addOnFailureListener {
+                            Toast.makeText(requireContext(), it.toString(), Toast.LENGTH_LONG)
+                                .show()
+                        }
                     clearForm()
                     showEventCreatedDialog()
                 }
-                .addOnFailureListener { exception -> Toast.makeText(mContext, "Failed with ${exception.message}", Toast.LENGTH_LONG)
-                    .show()
+                .addOnFailureListener { exception ->
+                    Toast.makeText(mContext, "Failed with ${exception.message}", Toast.LENGTH_LONG)
+                        .show()
                 }
         }
 
@@ -117,16 +163,36 @@ class GameCreateFragment:Fragment() {
     }
 
 
+    private fun showAvailableVenues() {
+        val db = Firebase.firestore
 
-    private fun showEventCreatedDialog(){
+        val date = Date(System.currentTimeMillis())
+        db.collectionGroup("venues").whereEqualTo(
+            "date", SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                .format(date)
+        ).get().addOnSuccessListener { query ->
+            if (!query.isEmpty) {
+                val doc = query.documents[0]
+                val path = doc.reference.path
+
+                Log.d("LOG", path)
+
+            } else {
+                Log.d("LOG", "Venue is free")
+            }
+        }
+    }
+
+    private fun showEventCreatedDialog() {
         MaterialAlertDialogBuilder(mContext)
             .setTitle("Information")
             .setMessage("Your Event has been created! Do you want to see it now?")
-            .setNegativeButton("No thanks"){_, _ -> findNavController().navigateUp()}
-            .setPositiveButton("Yes Please"){_, _-> }
+            .setNegativeButton("No thanks") { _, _ -> findNavController().navigateUp() }
+            .setPositiveButton("Yes Please") { _, _ -> }
             .show()
     }
-    private fun clearForm(){
+
+    private fun clearForm() {
         binding.edtiDate.clearComposingText()
         binding.editTime.clearComposingText()
         binding.editVenue.clearListSelection()
@@ -134,33 +200,39 @@ class GameCreateFragment:Fragment() {
         binding.edtNumberPlayers.clearComposingText()
         binding.audienceGroup.clearCheck()
     }
-    private fun selectDate():String{
+
+    private fun selectDate(): String {
         val edtDate = binding.edtiDate
         edtDate.setOnClickListener {
-            val datePicker = MaterialDatePicker.Builder.datePicker().
-            setTitleText("Select Date")
-                .setSelection(MaterialDatePicker
-                    .todayInUtcMilliseconds())
+            val datePicker = MaterialDatePicker.Builder.datePicker().setTitleText("Select Date")
+                .setSelection(System.currentTimeMillis())
+                .setSelection(
+                    MaterialDatePicker
+                        .todayInUtcMilliseconds()
+                )
                 .build()
 
 
             datePicker.addOnPositiveButtonClickListener {
                 val selectedDate = Date(it)
-                val formattedDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(selectedDate)
+                val formattedDate =
+                    SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(selectedDate)
                 edtDate.setText(formattedDate)
             }
 
             datePicker.showNow(parentFragmentManager, "date")
         }
 
-        return  edtDate.text.toString()
+        return edtDate.text.toString()
     }
 
-    private fun selectTime():String {
+    private fun selectTime(): String {
         val edtTime = binding.editTime
         edtTime.setOnClickListener {
             val timePicker = MaterialTimePicker.Builder()
                 .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setInputMode(MaterialTimePicker.INPUT_MODE_CLOCK)
+                .setHour(System.currentTimeMillis().toInt())
                 .setTitleText("Select Event Time")
                 .build()
 
@@ -172,19 +244,33 @@ class GameCreateFragment:Fragment() {
             timePicker.show(parentFragmentManager, "time")
         }
 
-        return  edtTime.text.toString()
+        return edtTime.text.toString()
     }
-    private fun buildVenuesItem(data: List<String>){
+
+    private fun buildVenuesItem(data: List<String>) {
         val selectedGame = binding.editVenue
-        val adapter = ArrayAdapter<String>(mContext, android.R.layout.simple_dropdown_item_1line, data);
+        val adapter =
+            ArrayAdapter<String>(mContext, android.R.layout.simple_dropdown_item_1line, data);
         selectedGame.setAdapter(adapter)
     }
 
-    private fun buildGamesItems(data: List<String>){
+    private fun buildUnitOfTime() {
+        val unitOfTimeEditText = binding.editDurationFormat
+        val adapter = ArrayAdapter<String>(
+            mContext, android.R.layout.simple_dropdown_item_1line, listOf<String>(
+                "Hour", "Minute"
+            )
+        )
+        unitOfTimeEditText.setAdapter(adapter)
+    }
+
+    private fun buildGamesItems(data: List<String>) {
         val selectedGame = binding.edtGame
-        val adapter = ArrayAdapter<String>(mContext, android.R.layout.simple_dropdown_item_1line, data);
+        val adapter =
+            ArrayAdapter<String>(mContext, android.R.layout.simple_dropdown_item_1line, data);
         selectedGame.setAdapter(adapter)
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
